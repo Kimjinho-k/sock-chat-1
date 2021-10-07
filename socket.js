d2506906f184140c9503c580754e6d823687d9e0
Kimjinho-k <ssi03029@gmail.com>
2021-10-07 18:53:25 +0900

    feat: Broadcasting message (socketio done)

--- socket.js
+++ socket.js
@@ -1,7 +1,9 @@
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("A user is now connected");
+
     socket.on("chat message", (msg) => {
+      io.emit("chat message", msg);
       console.log("message: ", msg);
     });
 
